cb6da26d72d73bea33a07cd7ca311719181d9199
Jorge Rodriguez <jorge.rodriguez@piksel.com>
2021-01-07 20:26:49 +0100

    added libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Text, Card, BaseTheme } from 'ui-example-library';
+import { myHelloWorldFunction } from 'core-example-library';
 
-function App() {
+interface Props {
+
+}
+
+const App = (props: Props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginTop: BaseTheme.space[6] }}>
+      <Card>
+        <Text value={myHelloWorldFunction('Jorge')} />
+      </Card>
     </div>
-  );
+  )
 }
 
 export default App;
